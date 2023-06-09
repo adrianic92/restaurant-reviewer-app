3d02de912b087449a66dba83faee69905ffcf1d5
Adrian <adrianic92@yahoo.com>
2023-06-09 12:15:55 -0700

    added to user/app and session controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,24 @@
 class UsersController < ApplicationController
+    skip_before_action :authorize, only: [:create, :display]
+
+    def create
+        user = User.create!(user_params)
+        session[:user_id] = user.id
+        render json: user, status: :created
+    end
+
+    def show
+        render json: @current_user
+    end
+
+    def display
+        users = User.all
+        render json: users
+    end
+
+    private
+
+    def user_params
+        params.permit(:username, :password, :password_confirmation, :name)
+    end
 end
