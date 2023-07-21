0c799db08b57c69e0fb7c5064877a0f71e820250
Adrian <adrianic92@yahoo.com>
2023-07-20 20:49:17 -0700

    added uniqueness to reviews

--- app/models/review.rb
+++ app/models/review.rb
@@ -3,7 +3,7 @@ class Review < ApplicationRecord
   belongs_to :restaurant
 
   validates :rating, presence: true, numericality: true, inclusion: { in: 1..5 }
-  validates :restaurant_id, uniqueness: true
+  validates :restaurant_id, uniqueness: { scope: :user_id}
   validates :description, presence: true
 
 end
